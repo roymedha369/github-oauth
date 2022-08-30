630f81d81e7ee4377540f715cb8d03a5e65df90e
roymedha369 <71056921+roymedha369@users.noreply.github.com>
2022-08-30 18:45:04 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  OAUTH_CLIENT: '1c008509f10e15a34aaf',
-  OAUTH_SECRET: '36e1b0f9a7a7227784394dc1b4f8775069891c41',
-  REDIRECT_API_URL: 'http://localhost:4200/home',
+  OAUTH_CLIENT: '',
+  OAUTH_SECRET: '',
+  REDIRECT_API_URL: '',
 };
 
 /*
